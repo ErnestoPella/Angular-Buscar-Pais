fbd2372527643a8b210984c73d6465b95b76fd84
ErnestoPella <104087343+ErnestoPella@users.noreply.github.com>
2023-01-24 19:57:29 -0500

    Visual de ver por capital y pais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     PaisModule,
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./por-capital.component.css']
 })
 export class PorCapitalComponent {
+
+
+  termino:string = "";
+  hayError: boolean = false;
+  paises: Country[] = [];
+
+  constructor(private paisService: PaisService){}
+
+
+  buscar( termino: string){  
+
+    this.hayError = false;
+    this.termino = termino;
+
+    this.paisService.buscarCapital(this.termino)
+    .subscribe((paises) => {   
+      this.paises = paises;
+    }, (err) => {
+      this.hayError = true;
+      this.paises = [];
+    }); 
+  }
 
 }
