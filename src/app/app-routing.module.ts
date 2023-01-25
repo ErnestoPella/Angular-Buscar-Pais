0f3ae369b0526703f4d33ffdfa1de48965f538fc
ErnestoPella <104087343+ErnestoPella@users.noreply.github.com>
2023-01-24 23:06:10 -0500

    terminado ver pais

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule,Routes } from "@angular/router";
 import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
 import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
 import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
+import { VerPaisComponent } from './pais/pages/ver-pais/ver-pais.component';
 
 
 const routes: Routes =[
@@ -23,7 +24,7 @@ const routes: Routes =[
     },
     {
         path: 'pais/:id',
-        component: PorPaisComponent,
+        component: VerPaisComponent,
         pathMatch: 'full'
     },
     {
