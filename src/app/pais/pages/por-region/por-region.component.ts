b8cedb35d28a48ca805c2087327a86b09a8e7a34
ErnestoPella <104087343+ErnestoPella@users.noreply.github.com>
2023-01-26 16:46:26 -0500

    por region done

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
   styleUrls: ['./por-region.component.css']
 })
-export class PorRegionComponent {
+
+
+export class PorRegionComponent{
+
+  regiones: string[] = ['africa','americas','asia','europe','oceania'];
+  regionActiva: string = '';
+  paises: Country[] = [];
+  hayError: boolean = false;
+  
+
+  constructor(private paisService: PaisService){}
+
+  getClaseCSS(region:string){
+    return (region === this.regionActiva) ? 'btn btn-primary': 'btn btn-outline-primary';
+  }
+
+  activarRegion(region: string){
+    
+    if (region === this.regionActiva) {
+      return;
+    }
+    
+    this.regionActiva = region;
+
+    this.paisService.getPaisPorRegion( region )
+    .subscribe(paises => this.paises = paises);
+      
+  }
 
 }
